6c1e6b2498b0a726237d8b0c14552a58a1729824
DenShykyta <wrkacc4@gmail.com>
2023-02-27 20:37:08 +0200

    ДЗ 7 (version 1.0)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({original,preview,description})=> {
+        return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`
+    }).join('');
+}
+
+// console.log(createGalleryMarkup(galleryItems));
+// console.log(galleryContainer);
+
+galleryContainer.addEventListener('click', onImageClick);
+function onImageClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") {
+    return;
+  };
+
+  basicLightbox.create(`
+		<img width="1400" height="900" src="${e.target.dataset.source}">
+	`).show();
+
+  // console.log(e.target.dataset.source);
+  // console.log(e.currentTurget);
+
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(({original,preview,description})=> {
+        return `
+<li class="gallery__item">
+    <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+</li>
+`
+    }).join('');
+}
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionPosition: 'bottom', captionDelay: 250 });
+
